fb4403ddb3d835f8437841c44dbae5f872b82c24
Lamin Samba <lamin.samba@hotmail.com>
2018-04-14 14:47:57 +0100

    LS : Add web project

---
+++ Delta/Delta.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Delta.Web.MvcApplication" Language="C#" %>
---
+++ Delta/Delta.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Delta.Web.Startup))]
+namespace Delta.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
